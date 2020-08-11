7637d89ac1948e34b9158c52dc3eb71fecf29409
agorararmard <aagouhar@efabless.com>
2020-08-12 01:28:36 +0200

    testsets for magic and openroad

--- travisCI/travisPrebuild.sh
+++ travisCI/travisPrebuild.sh
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+TOOL=$1
 export PDK_ROOT=$(pwd)/pdks
 export RUN_ROOT=$(pwd)
 echo $PDK_ROOT
