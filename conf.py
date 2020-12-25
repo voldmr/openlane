7e709bfb17715e7c3852988303973151d428be68
agorararmard <aagouhar@efabless.com>
2020-12-25 22:18:38 +0200

    [SPHINX] change theme and add the remaining docs

--- conf.py
+++ conf.py
@@ -62,8 +62,6 @@ templates_path = ['_templates']
 exclude_patterns = [
     '_build',
     'Thumbs.db',
-    'CONTRIBUTING.md',
-    'AUTHORS.md',
     # Files included in other rst files.
 ]
 
@@ -73,7 +71,7 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_symbiflow_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
